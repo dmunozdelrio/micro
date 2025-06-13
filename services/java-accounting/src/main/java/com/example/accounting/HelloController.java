bc9ac8ed87195710d5126dcf23be1b74795fb168
Diego Munoz del rio <85699761+dmunozdelrio@users.noreply.github.com>
2025-06-13 08:13:58 -0600

    Add Java Accounting service

---
+++ services/java-accounting/src/main/java/com/example/accounting/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.accounting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Accounting Service";
+    }
+}
